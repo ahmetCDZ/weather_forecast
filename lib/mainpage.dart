16869538965462f5519eafe893e31f175bd26c26
Ahmet Zincir <ahmetzincir27@gmail.com>
2022-10-26 21:21:46 +0300

    initState

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,18 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:weather_forecast/searchPage.dart';
 import 'package:http/http.dart' as http;
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
   String location = "Ankara";
+
   double temp = 0;
+
   final String apiKey = 'b16ca46383c5be7ffc134fefdae0ac48';
+
   var locationData;
 
   Future<void> getLocationWeather() async {
     locationData = await http.get(Uri.parse(
         'https://api.openweathermap.org/data/2.5/weather?q=$location,uk&appid=$apiKey'));
+    final locationDataParsed = jsonDecode(locationData.body);
+
+    setState(() {
+      temp = locationDataParsed['main'][("temp")];
+      location = locationDataParsed['name'];
+    });
+  }
+
+  @override
+  void initState() {
+    getLocationWeather();
+    super.initState();
   }
 
   @override
@@ -27,12 +49,6 @@ class MainPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                  onPressed: (() async {
-                    await locationData();
-                    print(locationData);
-                  }),
-                  child: Text('Hava')),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
--- lib/searchPage.dart
+++ lib/searchPage.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
